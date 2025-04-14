b4d798376ea245a788b95fd6a9d590bdd6198615
GyulyVGC <gyulyvgc99@gmail.com>
2025-04-14 11:02:09 +0200

    remove integration tests

--- tests/config/mod.rs
+++
@@ -1,9 +0,0 @@
-use appguard_server::config::Config;
-use appguard_server::constants::CONFIG_FILE;
-
-pub fn write_config_to_file(config: &Config) {
-    let json = serde_json::to_string(&config).unwrap();
-    std::fs::write(CONFIG_FILE, json).unwrap();
-
-    assert_eq!(Config::from_file(CONFIG_FILE).unwrap(), *config);
-}
