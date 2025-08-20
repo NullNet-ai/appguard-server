061f033e9b93020cf048ed84fea5310937de59e9
GyulyVGC <gyulyvgc99@gmail.com>
2025-08-20 11:47:17 +0200

    don't set timestamp field in upsert query to ip_infos table

--- src/db/store/ip_info.rs
+++ src/db/store/ip_info.rs
@@ -1,11 +1,9 @@
-use crate::helpers::get_timestamp_string;
 use crate::proto::appguard::AppGuardIpInfo;
 use serde_json::json;
 
 impl AppGuardIpInfo {
     pub(crate) fn to_json(&self) -> String {
         json!({
-            "timestamp": get_timestamp_string(),
             "ip": self.ip,
             "country": self.country,
             "asn": self.asn,
